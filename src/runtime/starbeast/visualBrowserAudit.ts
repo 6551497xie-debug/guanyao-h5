1b166fd388de27b8b2bc910ccd76c2b535a4ad01
Xie Yanjun <6551497xie@gmail.com>
2026-07-04 22:16:15 +0530

    test(starbeast): add browser visual audit helper

---
+++ src/runtime/starbeast/visualBrowserAudit.ts
@@ -0,0 +1,9 @@
+export function runStarbeastVisualAudit(): void {
+  console.log("VISUAL_AUDIT_START");
+  console.log("1. STARFIELD_RENDERED");
+  console.log("2. TOP_TEXT_VISIBLE");
+  console.log("3. LIGHT_AGGREGATION_ACTIVE");
+  console.log("4. STARBEAST_VISIBLE");
+  console.log("5. COLLAPSE_TRIGGER_READY");
+  console.log("VISUAL_AUDIT_END");
+}
